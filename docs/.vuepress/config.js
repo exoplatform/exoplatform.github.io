7e400d09fecf7392a28f245264fa89ea4e4ce1e9
Romain Dénarié <romain.denarie@exoplatform.com>
2024-02-16 10:56:50 +0100

    Add SAML documentation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -160,6 +160,7 @@ module.exports = {
             '/administration/indexing',
             '/administration/LDAP',
             '/administration/oauth-integration',
+            '/administration/saml-integration',
             '/administration/addons-management',
             '/administration/security',
             '/administration/jmx-rest-management'
